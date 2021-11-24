6e6e1bcdac9c7cc4d99f56553f64b2e487d44829
Adrian Frankowski <90799700+Hassagi@users.noreply.github.com>
2021-11-24 20:14:01 +0100

    Update Liczby doskonałe.m

--- Wolfram Mathematica/Liczby doskonałe.m
+++ Wolfram Mathematica/Liczby doskonałe.m
@@ -1,3 +1,20 @@
+(* Funkcja sprawdzająca czy podana liczba jest doskonała sposób 1 *)
+
+CzyDoskonała1[n_] := (Suma = 0;
+  For[i = 1, i <= n/2, i++, If[Mod[n, i] == 0, Suma = Suma + i]];
+    If[Suma == n, Return[True], Return[False]])
+
+(* Funkcja sprawdzająca czy podana liczba jest doskonała sposób 2 *)
+
+CzyDoskonała2[n_] := (p = Sqrt[n]; Suma = 1;
+  For[i = 2, i <= p, i++, If[Mod[n, i] == 0, Suma = Suma + i + n/i]];
+    If[Sqrt[n] - Floor[Sqrt[n]] == 0, Suma = Suma - Sqrt[n]];
+      If[Suma == n, Return[True], Return[False]])
+      
+(* Porównanie czasów działania obu metod *)
+
+Print[Timing[CzyDoskonała1[33550336]], " ",Timing[CzyDoskonała2[33550336]]]
+
 (* Funkcja zwracająca n początkowych liczb doskonałych *)
 
 Doskonała[n_] := (L = {}; m = n; potęga2 = 2;
