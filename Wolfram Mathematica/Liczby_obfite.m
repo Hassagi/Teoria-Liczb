023c0e4014e2ee1a15137579acae624a9b3d5eda
Adrian Frankowski <90799700+Hassagi@users.noreply.github.com>
2021-11-28 13:35:49 +0100

    Update Liczby_obfite.m

--- Wolfram Mathematica/Liczby_obfite.m
+++ Wolfram Mathematica/Liczby_obfite.m
@@ -1,1 +1,8 @@
-(* funkcja *)
+(* funkcja zwracająca wszystkie liczby obfite z przedziału [1, n] *)
+
+SumaDzielników[n_] := DivisorSigma[1, n]
+Obfitość[n_] := SumaDzielników[n] - 2 n
+
+Obfite[m_] := (L = {};
+  For[n = 1, n <= m, n++, If[Obfitość[n] > 0, AppendTo[L, n]]]; 
+  Return[L])
